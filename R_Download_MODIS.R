d960297e8bce662426775abdb84417780aa6f982
jziemer1996 <jz199623@gmail.com>
2020-06-04 17:59:33 +0200

    cpoied coles original

--- R_Download_MODIS.R
+++ R_Download_MODIS.R
@@ -5,42 +5,28 @@
 # ------------------------------------------------------------------------------------------------ #
 # Author: Cole Krehbiel
 # Last Updated: 11/14/2019
-# Modified: Marlin Mueller & Jonas Ziemer
 # ------------------------------------------------------------------------------------------------ #
-
 # Check for required packages, install if not previously installed
-
 if ("sys" %in% rownames(installed.packages()) == FALSE) {install.packages("sys")}
 if ("getPass" %in% rownames(installed.packages()) == FALSE) { install.packages("getPass")}
 if ("httr" %in% rownames(installed.packages()) == FALSE) { install.packages("httr")}
 
 # Load necessary packages into R
-
 library(sys)
 library(getPass)
 library(httr)
-
 # ---------------------------------SET UP ENVIRONMENT--------------------------------------------- #
-
 # IMPORTANT: Update the line below if you want to download to a different directory (ex: "c:/data/")
-
-# Set Jonas' dir to download files to
-dl_dir <- Sys.getenv("C:/Users/jz199/Documents/Studium/")                                 
-
-# Set Marlins' dir to download files to
-# dl_dir <- Sys.getenv("HOME")                                 
-
+dl_dir <- Sys.getenv("HOME")                                 # Set dir to download files to
 setwd(dl_dir)                                                # Set the working dir to the dl_dir
-usr <- file.path(Sys.getenv("USERNAME"))                  # Retrieve home dir (for netrc file)
+usr <- file.path(Sys.getenv("USERPROFILE"))                  # Retrieve home dir (for netrc file)
 if (usr == "") {usr = Sys.getenv("HOME")}                    # If no user profile exists, use home
 netrc <- file.path(usr,'.netrc', fsep = .Platform$file.sep)  # Path to netrc file
 
 # ------------------------------------CREATE .NETRC FILE------------------------------------------ #
-
 # If you already have a .netrc file with your Earthdata Login credentials stored in your home
 # directory, this portion will be skipped. Otherwise you will be prompted for your NASA Earthdata
 # Login Username/Password and a netrc file will be created to store your credentials (in home dir)
-
 if (file.exists(netrc) == FALSE || grepl("urs.earthdata.nasa.gov", readLines(netrc)) == FALSE) {
   netrc_conn <- file(netrc)
   
@@ -52,18 +38,17 @@ if (file.exists(netrc) == FALSE || grepl("urs.earthdata.nasa.gov", readLines(net
 }
 
 # ---------------------------CONNECT TO DATA POOL AND DOWNLOAD FILES------------------------------ #
-
 # Below, define either a single link to a file for download, a list of links, or a text file
 # containing links to the desired files to download. For a text file, there should be 1 file link
 # listed per line. Here we show examples of each of the three ways to download files.
 # **IMPORTANT: be sure to update the links for the specific files you are interested in downloading.
 
-# 1. Single file:
+# 1. Single file (this is just an example link, replace with your desired file to download):
 files <- "https://e4ftl01.cr.usgs.gov/MOLT/MOD13A3.006/2020.03.01/MOD13A3.A2020061.h20v12.006.2020100130510.hdf"
 
-# 2. List of files:
-#files <- c("",
-#           "")
+# 2. List of files (these are just example links, replace with your desired files to download:
+#files <- c("https://e4ftl01.cr.usgs.gov/MOLA/MYD09GA.006/2002.07.06/MYD09GA.A2002187.h10v04.006.2015149103018.hdf",
+#           "https://e4ftl01.cr.usgs.gov/MOLT/MOD11A1.006/2000.03.09/MOD11A1.A2000069.h00v08.006.2015057070313.hdf")
 
 # 3. Textfile containing links (just an example, replace with your text file location):
 #files <- readLines("C:/datapool_downloads/URL_file_list.txt", warn = FALSE)
